e6cd9f23dcedb5bd70acad25344973ffeb832f29
Kansamu <zhaiyuxiao1@foxmail.com>
2023-02-18 16:30:45 +0800

    增加导航栏

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,4 +1,19 @@
 module.exports = {
     title: 'PaperLand-绘岛',
-    description: 'An island of harmony'
+    description: 'Island-Of-Harmony'
+  }
+
+  module.exports = {
+    themeConfig: {
+      logo: 'http://api.oss.bj.kansamu.com/paperland.png',
+    }
+  }
+
+  module.exports = {
+    themeConfig: {
+      nav: [
+        { text: 'External', link: 'https://google.com', target:'_self', rel:'' },
+        { text: 'Guide', link: '/guide/', target:'_blank' }
+      ]
+    }
   }
