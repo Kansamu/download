b26b9661ef1d990c0681e95e823c4ace52979f1e
Kansamu <zhaiyuxiao1@foxmail.com>
2023-02-18 17:22:05 +0800

    修改导航栏

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -2,3 +2,15 @@ module.exports = {
     title: 'PaperLand-绘岛',
     description: '温馨友爱小社区'
   }
+
+
+
+// .vuepress/config.js
+module.exports = {
+  themeConfig: {
+    nav: [
+      { text: 'External', link: 'https://google.com', target:'_self', rel:'' },
+      { text: 'Guide', link: '/guide/', target:'_blank' }
+    ]
+  }
+}
