f000c7efd1587214c2f66b4b632faa438384ea8e
Kansamu <zhaiyuxiao1@foxmail.com>
2023-02-20 14:47:57 +0800

    完善

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -4,6 +4,7 @@ module.exports = {
 
     themeConfig: {
       sidebar: 'auto',
+      displayAllHeaders: true,
       nav: [
         { text: '绘岛皮肤站', link: 'https://skin.kansamu.com/', target:'_blank' },
       ]
